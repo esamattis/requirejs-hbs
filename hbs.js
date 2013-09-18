53d223d2bc93cd42caa87e575d8dc56f198f2294
Benoît Zugmeyer <bzugmeyer@gmail.com>
2013-09-18 20:05:31 +0200

    Separate hbs client loader and builder
    In order to be able to require different modules at build time and in
    the client side, we have to split the client loader and the builder.
    
    This way, we require a 'handlebars-compiler' for the builder, so we can
    set the 'handlebars' module to the runtime-only file at build time and
    when using the built package.
    
    One other benefit is the final bundle overhead will be smaller in size.
    
    It appears that both files can't share any code, so we have to specify
    the extension on each one.

--- hbs.js
+++ hbs.js
@@ -1,8 +1,9 @@
-define(["handlebars"], function(Handlebars) {
-  var buildMap = {},
-      templateExtension = ".hbs";
+define(["handlebars"], function (Handlebars) {
+  var templateExtension = ".hbs";
 
   return {
+
+    pluginBuilder: "./hbs-builder",
 
     // http://requirejs.org/docs/plugins.html#apiload
     load: function (name, parentRequire, onload, config) {
@@ -10,34 +11,13 @@ define(["handlebars"], function(Handlebars) {
       // Get the template extension.
       var ext = (config.hbs && config.hbs.templateExtension ? config.hbs.templateExtension : templateExtension);
 
-      if (config.isBuild) {
-        // Use node.js file system module to load the template.
-        // Sorry, no Rhino support.
-        var fs = nodeRequire("fs");
-        var fsPath = config.dirBaseUrl + "/" + name + ext;
-        buildMap[name] = fs.readFileSync(fsPath).toString();
-        onload();
-      } else {
-        // In browsers use the text-plugin to the load template. This way we
-        // don't have to deal with ajax stuff
-        parentRequire(["text!" + name + ext], function(raw) {
-          // Just return the compiled template
-          onload(Handlebars.compile(raw));
-        });
-      }
+      // In browsers use the text-plugin to the load template. This way we
+      // don't have to deal with ajax stuff
+      parentRequire(["text!" + name + ext], function (raw) {
+        // Just return the compiled template
+        onload(Handlebars.compile(raw));
+      });
 
-    },
-
-    // http://requirejs.org/docs/plugins.html#apiwrite
-    write: function (pluginName, name, write) {
-      var compiled = Handlebars.precompile(buildMap[name]);
-      // Write out precompiled version of the template function as AMD
-      // definition.
-      write(
-        "define('hbs!" + name + "', ['handlebars'], function(Handlebars){ \n" +
-          "return Handlebars.template(" + compiled.toString() + ");\n" +
-        "});\n"
-      );
     }
 
   };
